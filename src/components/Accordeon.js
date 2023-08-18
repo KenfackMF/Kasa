16037c97ff0512eadfe2ef17a31a9404f7570141
KenfackMF <francis.kenfack@gmail.com>
2023-08-18 21:48:24 +0200

    fade out

--- src/components/Accordeon.js
+++ src/components/Accordeon.js
@@ -3,22 +3,21 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronUp } from "@fortawesome/free-solid-svg-icons";
 
 const Accordeon = ({ title, content }) => {
-  // State pour gérer la visibilité
+  // State pour gérer la visibilité et la rotation
   const [active, setActive] = useState(false);
 
-  // Fonction pour basculer la visibilité
   const toggleVisibility = () => {
     setActive(!active);
   };
 
   return (
-    <div className="accordeon">
-      <div className={`bande-rouge ${active ? "active" : "inactive"}`} onClick={toggleVisibility}>
+    <div className={`accordeon ${active ? "active" : ""}`}>
+      <div className="bande-rouge" onClick={toggleVisibility}>
         <h4>{title}</h4>
         <FontAwesomeIcon icon={faChevronUp} className={`arrow ${active ? "rotate" : ""}`} />
       </div>
       {/* Affiche le contenu si l'accordéon est actif */}
-      {active && <div className="container-texte">{content}</div>}
+      <div className={`container-texte ${active ? "active" : ""}`}>{content}</div>
     </div>
   );
 };
