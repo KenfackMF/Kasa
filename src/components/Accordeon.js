361934635bd1461331c9812fccd179489e7c2e86
KenfackMF <francis.kenfack@gmail.com>
2023-08-22 00:50:47 +0200

    animation et query ok

--- src/components/Accordeon.js
+++ src/components/Accordeon.js
@@ -3,21 +3,22 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronUp } from "@fortawesome/free-solid-svg-icons";
 
 const Accordeon = ({ title, content }) => {
-  // State pour gérer la visibilité et la rotation
+  // State pour gérer la visibilité
   const [active, setActive] = useState(false);
 
+  // Fonction pour basculer la visibilité
   const toggleVisibility = () => {
     setActive(!active);
   };
 
   return (
-    <div className={`accordeon ${active ? "active" : ""}`}>
-      <div className="bande-rouge" onClick={toggleVisibility}>
+    <div className="accordeon">
+      <div className={`bande-rouge ${active ? "active" : "inactive"}`} onClick={toggleVisibility}>
         <h4>{title}</h4>
         <FontAwesomeIcon icon={faChevronUp} className={`arrow ${active ? "rotate" : ""}`} />
       </div>
       {/* Affiche le contenu si l'accordéon est actif */}
-      <div className={`container-texte ${active ? "active" : ""}`}>{content}</div>
+      <div className={`container-texte ${active ? "active" : "inactive"}`}>{content}</div>
     </div>
   );
 };
